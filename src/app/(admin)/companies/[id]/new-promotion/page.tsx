44148055c5bf7a22aec8c27a65b7b25929418995
Dmytro Medvediev <dsisitkin@gmail.com>
2024-10-24 11:34:08 +0200

    Update page.tsx

--- src/app/(admin)/companies/[id]/new-promotion/page.tsx
+++ src/app/(admin)/companies/[id]/new-promotion/page.tsx
@@ -1,16 +1,29 @@
-'use client';
-
 import React from 'react';
 import PromotionForm from '@/app/components/promotion-form';
+import { getCompanies, getCompany } from '@/app/lib/api';
 
 export interface PageProps {
   params: { id: string };
 }
 
-export default function Page({ params }: PageProps) {
+export default async function Page({ params }: PageProps) {
+  const company = await getCompany(params.id);
+
+  if (!company) {
+    return <div>Company not found</div>;
+  }
+
   return (
     <div className="py-6 px-10">
+      <h1>{company.title}</h1>
       <PromotionForm companyId={params.id} />
     </div>
   );
+}
+
+export async function generateStaticParams() {
+  const companies = await getCompanies(); 
+  return companies.map((company) => ({
+    id: company.id.toString(),
+  }));
 }
